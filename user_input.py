1c072fe4043a9b2c75b48537a3b451073abba2ff
altasheheth <mweumartin@gmail.com>
2024-03-18 23:02:11 +0300

    Week1 assignment

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#Asks for person's name
+name = input(' Hi What is your name: ')
+
+# Asks for person's age
+age = input('How old are you: ')
+
+# Asks for person's location
+location = input('What is your current location: ')
+
+# Outputs the captured persons name, age and location
+print('Hello', name ,'you are', age,'years old and you are in', location)
